08a159eb43b931ff75a4bf17d3439c7f56d8e29f
Edwin Carlsson <edwin98carlsson@gmail.com>
2019-04-23 13:34:39 +0200

    fix gitignorer

--- build/launcher.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cat /home/robot/java/splash.txt
-java -Xms64m -Xmx64m -XX:+UseSerialGC -Xshare:off -noverify -cp "/home/robot/java/programs/shipping-system-2.5.3-all.jar" com.bth.ShippingSystemMain.ShippingSystemMain
-exit $?
